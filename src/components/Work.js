1f5115e921bff4f5cccdb1d3c24c45936d6186a4
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2020-10-01 15:23:31 -0300

    adding amazon clone

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,6 +3,7 @@ import WorkCard from './WorkCard';
 import { Zoom } from 'react-reveal';
 
 import CapillaImg from '../assets/img/capilla-marin.png';
+import AmazonClone from '../assets/img/amazon-clone.png';
 import GolfamImg from '../assets/img/golfam.png';
 import RockImg from '../assets/img/rock-paper-scissors.png';
 import CountriesImg from '../assets/img/countries.png';
@@ -11,7 +12,7 @@ import CountriesImg from '../assets/img/countries.png';
 import './Work.css';
 
 const Work = () => {
-    return(
+    return (
         <div id="workSection" className="section work-section">
             <div className="title">
                 <Zoom>
@@ -21,51 +22,64 @@ const Work = () => {
             <div className="work__container">
                 <div className="work-card__container">
                     <Zoom>
-                        <WorkCard 
-                        title="Capilla Marin" 
-                        img={CapillaImg} 
-                        link="https://capillamarin.org.ar" 
-                        github="https://github.com/mariscottin/capillamarin"
-                        subtitle="Church website with custom-made admin portal."
-                        details={['Node JS for backend', 'EJS as View Engine', 'PostgreSQL as DB', 'AWS S3 for media storage', 'Deployed to Heroku']}
+                        <WorkCard
+                            title="Capilla Marin"
+                            img={CapillaImg}
+                            link="https://capillamarin.org.ar"
+                            github="https://github.com/mariscottin/capillamarin"
+                            subtitle="Church website with custom-made admin portal."
+                            details={['Node JS for backend', 'EJS as View Engine', 'PostgreSQL as DB', 'AWS S3 for media storage', 'Deployed to Heroku']}
                         />
                     </Zoom>
                 </div>
                 <div className="work-card__container">
                     <Zoom>
-                        <WorkCard 
-                        title="Golfam Live Prototype" 
-                        img={GolfamImg} 
-                        link="https://golfamfrontend.z19.web.core.windows.net/"
-                        github="https://github.com/mariscottin/golfam"
-                        subtitle="A golf web app prototype for amateur players."
-                        details={['React JS for frontend', 'Node JS for backend', 'Mongodb as DB', 'REST API deployed to Heroku', 'React App deployed to Azure']}
-                        username="113113"
-                        password="113113113"
+                        <WorkCard
+                            title="Amazon Clone"
+                            img={AmazonClone}
+                            link="https://clone-a5a50.web.app/"
+                            github="https://github.com/mariscottin/amazon-clone"
+                            subtitle="Amazon clone with payment functionality"
+                            details={['React JS', 'Firestore as DB', 'Cloud Functions and authentication with Firebase', 'Deployed to Firebase']}
                         />
                     </Zoom>
                 </div>
                 <div className="work-card__container">
                     <Zoom>
-                        <WorkCard 
-                        title="Rock Paper Scissors" 
-                        img={RockImg} 
-                        link="https://rock-paper-scissors-iota.vercel.app/"
-                        github="https://github.com/mariscottin/rock_paper_scissors"
-                        subtitle="A frontendmentor.io advanced challenge."
-                        details={['React JS', 'Local browser storage to save score', 'Deployed to Vercel']}
+                        <WorkCard
+                            title="Golfam Live Prototype"
+                            img={GolfamImg}
+                            link="https://golfamfrontend.z19.web.core.windows.net/"
+                            github="https://github.com/mariscottin/golfam"
+                            subtitle="A golf web app prototype for amateur players."
+                            details={['React JS', 'Node JS for backend', 'Mongodb as DB', 'REST API deployed to Heroku', 'React App deployed to Azure']}
+                            username="113113"
+                            password="113113113"
+                        />
+                    </Zoom>
+                </div>
+
+                <div className="work-card__container">
+                    <Zoom>
+                        <WorkCard
+                            title="Rock Paper Scissors"
+                            img={RockImg}
+                            link="https://rock-paper-scissors-iota.vercel.app/"
+                            github="https://github.com/mariscottin/rock_paper_scissors"
+                            subtitle="A frontendmentor.io advanced challenge."
+                            details={['React JS', 'Local browser storage to save score', 'Deployed to Vercel']}
                         />
                     </Zoom>
                 </div>
                 <div className="work-card__container">
                     <Zoom>
-                        <WorkCard 
-                        title="Countries App" 
-                        img={CountriesImg} 
-                        link="https://rest-countries-app.vercel.app/"
-                        github="https://github.com/mariscottin/rest-countries-app"
-                        subtitle="A frontendmentor.io advanced challenge."
-                        details={['React JS', 'Fetch requests to restcountries.eu API', 'Deployed to Vercel']}
+                        <WorkCard
+                            title="Countries App"
+                            img={CountriesImg}
+                            link="https://rest-countries-app.vercel.app/"
+                            github="https://github.com/mariscottin/rest-countries-app"
+                            subtitle="A frontendmentor.io advanced challenge."
+                            details={['React JS', 'Fetch requests to restcountries.eu API', 'Deployed to Vercel']}
                         />
                     </Zoom>
                 </div>
